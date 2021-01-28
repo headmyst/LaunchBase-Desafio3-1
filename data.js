7271b8b3e80fb71a814dc2d6c9e08077c8f0bf39
Ícaro Costa <babayagadeveloper@gmail.com>
2021-01-28 08:40:19 -0300

    Progress to 3-3

---
+++ data.js
@@ -0,0 +1,27 @@
+module.exports = [
+    {
+        id: "por-que-escrever-testes-automatizados/",
+        title: "Porque escrever testes automatizados",
+        image: "content/images/2019/03/Testes-automatizados--Experie-ncia-pessoal-.png",
+        author: "Hugo Ribeiro",
+        duration: "4 min" 
+     },
+
+     {
+        id: "ambiente-desenvolvimento-javascript/",
+        title: "Ambiente Javascript: Dicas, VSCode e terminal",
+        image: "content/images/2018/12/ambiente-desenvolvimento-javascript.png",
+        author: "Diego Fernandes",
+        duration: "4 min",
+        featured: true
+      },
+
+     {
+        id: "ssr-nextjs-reactjs",
+        title: "Server-side rendering (SSR) com ReactJS e Next.js",
+        image: "content/images/2018/12/ssr-nextjs-reactjs.png",
+        author: "Diego Fernandes",
+        duration: "4 min",
+        featured: true
+     }
+]
--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const nunjucks = require('nunjucks')
 
 const server = express()
 
+//O ./ referencia a um arquivo da raiz do projeto
+const videos = require("./data")
+
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false,
+    noCache: true   
 })
 
 //Criando Rotas
@@ -17,7 +22,24 @@ server.get("/", function(req, res){
 })
 
 server.get("/courses", function(req, res){
-    return res.render("courses")
+    return res.render("courses", {items: videos})
+})
+
+server.get("/video", function(req, res){
+//Crio uma variavel de nome id para definir ela com o código da url do vídeo
+    const id = req.query.id
+
+//Declaro uma váriavel com uma função que vai percorrer o array dos vídeos para encontar os id's.
+//========= TENTAR IMPLEMENTAR UM FOR AO INVÉS DE IF =========
+    const video = videos.find(function(video){
+        if (video.id == id)
+            return true    
+    })
+    if (!video.id) {
+        return("Video not found!")
+    }
+
+    return res.render("video", {video})
 })
 
 //Essa rota é direcionada a uma página não existente, famoso erro 404
@@ -26,6 +48,6 @@ server.use(function(req, res){
 })
 
 //Definindo Porta
-server.listen(5000, function(){
+server.listen(4000, function(){
     console.log("Server is running")
 })
